e87594612bb15204c7bf48a588c36e65fce306e3
Ricardo Bastardo <ric.ucvmicrop@gmail.com>
2018-11-30 09:27:35 -0400

    When we visit a thread page, then we see the replies

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -15,5 +15,23 @@
             </div>
         </div>
     </div>
+
+
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+
+            @foreach($thread->replies as $reply)
+            <div class="card">
+                
+                <div class="card-body">
+                  
+                    {{ $reply->body }}
+
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+
 </div>
 @endsection
---
+++ tests/Feature/ReadThreadsTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ReadThreadsTest extends TestCase
+{
+
+    use DatabaseMigrations;
+
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->thread = factory('App\Thread')->create();  
+
+    }
+
+    
+   /** @test */
+    public function a_user_can_view_all_threads()
+    {
+         
+        $response = $this->get('/threads')
+                    ->assertSee($this->thread->title);
+
+    }
+
+
+    /** @test */
+    function a_user_can_read_a_single_thread()
+    {
+        
+        $this->get('/threads/'.$this->thread->id)
+                ->assertSee($this->thread->title); 
+    }
+
+    /** @test */
+    function a_user_can_read_replies_that_are_associated_with_a_thread()
+    {
+        //Given we have a thread
+        //And that thread includes replies
+        $reply = factory('App\Reply')
+                ->create(['thread_id' => $this->thread->id]);
+        //When we visit a thread page
+        //The we should see the replies
+        $this->get('/threads/'.$this->thread->id)
+                ->assertSee($reply->body); 
+       
+    }
+
+
+
+}
