400699acf4df80002847ee623ec44e190a852612
Ricardo Bastardo <ric.ucvmicrop@gmail.com>
2018-12-02 00:43:21 -0400

    method create() changed by make(), in a factory, users adds a replies to the thread

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -15,10 +15,7 @@ class ParticipateInForumTest extends TestCase
     {
         $this->expectException('Illuminate\Auth\AuthenticationException');
         
-        $thread = factory('App\Thread')->create();
-
-        $reply = factory('App\Reply')->create();
-        $this->post($thread->path().'/replies', $reply->toArray());
+        $this->post('/threads/1/replies',[]);
 
     }
 
@@ -28,14 +25,14 @@ class ParticipateInForumTest extends TestCase
     function an_authenticated_user_may_participate_in_forum_threads()
     {
         //Given we have a authenticated user
-        $user = factory('App\User')->create(); 
+        
         $this->be($user = factory('App\User')->create());
 
         //And en existing thread
         $thread = factory('App\Thread')->create();
 
         //When the user adds a reply to the thread
-        $reply = factory('App\Reply')->create();
+        $reply = factory('App\Reply')->make();
         $this->post($thread->path().'/replies', $reply->toArray());
 
         //The their reply should be included on teh page. 
