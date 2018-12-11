78e2556ee8bec814926d09306a8708fc8b72753e
Ricardo Bastardo <ric.ucvmicrop@gmail.com>
2018-12-10 23:19:10 -0400

    User can create threads OK

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -9,19 +9,21 @@
 
                 <div class="card-body">
                   
-                   <form action="">
+                   <form method = "POST" action="/threads">
+                        
+                        {{ csrf_field() }}
 
                         <div class="form-group">
                             <label for = "title">Title:</label>
-                            <input type="text" class="form-control" id = "title" placeholder = "title">
+                            <input type="text" class="form-control" id = "title" name = "title">
                         </div>
 
                         <div class="form-group">
                             <label for = "body">Body:</label>
-                            <textarea name="body" id = "body" class="form-control"> </textarea>
+                            <textarea name="body" id = "body" class="form-control" rows = "8" > </textarea>
                         </div>
 
-                        <button type = "submit" class = "btn btn-default">Publish</button>
+                        <button type = "submit" class = "btn btn-primary">Publish</button>
 
                    </form>
 
