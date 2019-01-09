4ec9f158d2c52bb1637dfeca72fb87a42b57e832
Ricardo Bastardo <ric.ucvmicrop@gmail.com>
2019-01-09 12:07:31 -0400

    test guests cannot see the create thread page, delete

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -13,26 +13,17 @@ class CreateThreadsTest extends TestCase
     /** @test */
     function guest_may_not_create_threads()
     {
+        $this->withExceptionHandling();
 
-        $this->withExceptionHandling()
-                ->post('/threads')
+        $this->get('/threads/create')
                 ->assertRedirect('/login');
 
-        // $this->expectException('Illuminate\Auth\AuthenticationException');
+        $this->post('/threads')
+                ->assertRedirect('/login');
 
-        // $thread = make('App\Thread');
-
-        // $this->post('/threads', $thread->toArray());
     }
     
 
-    /** @test */
-    function guests_cannot_see_the_create_thread_page()
-    {
-        $this->withExceptionHandling()
-                ->get('/threads/create')
-                ->assertRedirect('/login');
-    }
 
     /** @test */
     function an_authenticated_user_can_create_new_forum_threads()
